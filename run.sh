979424d49b37a87c97c21f2d071ce81c8f6545b2
Ubuntu <ubuntu@ip-172-31-11-91.ap-south-2.compute.internal>
2026-09-16 10:55:54 +0000

    Add Docker configuration

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ echo "==========================================================================
 
 export PATH=$PATH:/usr/local/bin:/Library/Frameworks/Python.framework/Versions/3.12/bin
 
-/usr/local/bin/python3 -m uvicorn main:app --host 0.0.0.0 --port 8080 --reload
+python -m uvicorn main:app --host 0.0.0.0 --port 8080 --reload
